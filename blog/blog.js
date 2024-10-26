a1d6a82fc67aa5d1e8706ba6313b8704707094e0
kylemdavies <dav19010@byui.edu>
2024-10-26 14:22:02 -0600

    dumb quotation marks

--- blog/blog.js
+++ blog/blog.js
@@ -31,7 +31,7 @@ const articles = [
 		title: "Belgariad Book One: Pawn of Prophecy",
 		date: "Feb 12, 2022",
 		description:
-		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
 		imgSrc:
 		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
 		imgAlt: "Book cover for Pawn of Prophecy",
