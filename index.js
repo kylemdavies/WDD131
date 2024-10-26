7a827d5e525e7efdb46de98b4e32983e771a6985
kylemdavies <dav19010@byui.edu>
2024-10-26 16:56:27 -0600

    button dropdown

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function dropdown() {
-    document.getElementByID("myDropdown").classList.toggle("show");
-  }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }  
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
@@ -16,4 +16,4 @@ function dropdown() {
         }
       }
     }
-  }
+}
