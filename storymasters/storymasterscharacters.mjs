69191c3b5870637c34195ac71adfa20aa0bc22e9
kylemdavies <dav19010@byui.edu>
2024-12-09 12:14:55 -0700

    Basics for Storymasters project

---
+++ storymasters/storymasterscharacters.mjs
@@ -0,0 +1,52 @@
+const characters = [
+	{
+		name: "Roaden",
+        species: "Half-Elf",
+        subspecies: "Half-Elf",
+        job: "Rogue",
+        subjob: "",
+        background: "Spy (Enforcer)",
+        level: "1",
+        personality: "I am always calm, no matter what the situation. I never raise my voice or let my emotions control me.",
+        ideal: "People. I’m loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care. (Neutral)",
+        bond: "I’m guilty of a terrible crime. I hope I can redeem myself for it.",
+        flaw: "I turn tail and run when things look bad.",
+        traits: [
+            "Darkvision", "Fey Ancestry"
+        ],
+        languages: [
+            "Common", "Elvish", "Dwarvish"
+        ],
+        allies: [
+            "The Noble Man"
+        ],
+        backstory: 
+        `Roaden was shund as a child and as such only found friendship on the streets, one time he got caught pilffering important documents from a noble man.
+        The noble man instead of punishing him had Roaden work for him as a spy and Enforcer`
+	},
+	{
+		name: "Dominque (Dom)",
+        species: "Human",
+        subspecies: "Variant",
+        job: "Fighter",
+        subjob: "",
+        background: "Soldier (Officer)",
+        level: "1",
+        personality: "I'm haunted by memories of war. I can't get the images of violence out of my mind.",
+        ideal: "Independence. When people follow orders blindly, they embrace a kind of tyranny. (Chaotic)",
+        bond: "I'll never forget the crushing defeat my company suffered or the enemies who dealt it.",
+        flaw: "My hatred of my enemies is blind and unreasoning.",
+        traits: [
+            "Seond Wind", "Fighting Style: Interception", "Military Rank", "Alert"
+        ],
+        languages: [
+            "Common", "Elvish"
+        ],
+        allies: [
+            "Survivors of my unit", "City guards"
+        ],
+        backstory: 
+        `Dom was an officer in the army of his city, the enemy got the jump on his unit and a lot of them died. Now he freelances for the city and serves with
+        the city guard on occasion and has made friends with many of them.`
+	}
+]
