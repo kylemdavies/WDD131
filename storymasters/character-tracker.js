21875196cace0b273122c8940a517ad1773870ce
kylemdavies <dav19010@byui.edu>
2024-12-09 15:02:13 -0700

    more to personalities thinking about trait or ally buttons to filter

--- storymasters/character-tracker.js
+++ storymasters/character-tracker.js
@@ -30,6 +30,17 @@ let n = 0;
     });
   }
 
+  function addList(list, itemBox) {
+    list.forEach(item => {
+      const button = document.createElement('button');
+      button.classList.add('item-button');
+      button.setAttribute('type', 'button');
+      button.textContent = item;
+      button.addEventListener('click', () => filterByitem(item));
+      itemBox.appendChild(button);
+    });
+  }
+
 const characterList = document.getElementById('character-list');
 
 function createcharacterListItem(character) {
@@ -37,11 +48,19 @@ function createcharacterListItem(character) {
     characterDiv.classList.add('character')
 
     characterDiv.innerHTML = `
-        <div id="character-${n}" class="tag-buttons-box"></div>
-        <h3 class="character-name">Name: ${character.name}</h3>
-        <h3 class="character-name">Name: ${character.name}</h3>
-        
+        <div class="character-personalities">
+            <h3 class="character-name">Name: ${character.name}</h3>
+            <h3 class="character-personality">Personality: ${character.personality}</h3>
+            <h3 class="character-ideals">Ideals: ${character.ideals}</h3>
+            <h3 class="character-bonds">Bonds: ${character.bonds}</h3>
+            <h3 class="character-flaws">Flaws: ${character.flaws}</h3>
+        </div>
+        <div class="character-being">
+            <h3 class="character-species">Species: ${character.species}</h3>
+            <h3 class="character-subspecies">Subspecies: ${character.subspecies}</h3>
+        </div>
         <p class="character-description">${character.description}</p>
+        <div id="character-${n}" class="tag-buttons-box"></div>
     `;
     return characterDiv;
   }
--- storymasters/storymasterscharacters.mjs
+++ storymasters/storymasterscharacters.mjs
@@ -2,15 +2,15 @@ const characters = [
 	{
 		name: "Roaden",
         species: "Half-Elf",
-        subspecies: "Half-Elf",
+        subspecies: "None",
         job: "Rogue",
-        subjob: "",
+        subjob: "None",
         background: "Spy (Enforcer)",
         level: "1",
         personality: "I am always calm, no matter what the situation. I never raise my voice or let my emotions control me.",
-        ideal: "People. I’m loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care. (Neutral)",
-        bond: "I’m guilty of a terrible crime. I hope I can redeem myself for it.",
-        flaw: "I turn tail and run when things look bad.",
+        ideals: "People. I’m loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care. (Neutral)",
+        bonds: "I’m guilty of a terrible crime. I hope I can redeem myself for it.",
+        flaws: "I turn tail and run when things look bad.",
         traits: [
             "Darkvision", "Fey Ancestry"
         ],
@@ -32,13 +32,13 @@ const characters = [
         species: "Human",
         subspecies: "Variant",
         job: "Fighter",
-        subjob: "",
+        subjob: "None",
         background: "Soldier (Officer)",
         level: "1",
         personality: "I'm haunted by memories of war. I can't get the images of violence out of my mind.",
-        ideal: "Independence. When people follow orders blindly, they embrace a kind of tyranny. (Chaotic)",
-        bond: "I'll never forget the crushing defeat my company suffered or the enemies who dealt it.",
-        flaw: "My hatred of my enemies is blind and unreasoning.",
+        ideals: "Independence. When people follow orders blindly, they embrace a kind of tyranny. (Chaotic)",
+        bonds: "I'll never forget the crushing defeat my company suffered or the enemies who dealt it.",
+        flaws: "My hatred of my enemies is blind and unreasoning.",
         traits: [
             "Seond Wind", "Fighting Style: Interception", "Military Rank", "Alert"
         ],
